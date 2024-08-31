47c2a551b627bdc9984efdb0bd049466c7bcacda
ANURAGSINGHDK <anuragsinghdk@gmail.com>
2024-08-31 18:25:40 +0530

    pattern program 3and 4

---
+++ cross.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    printf("Enter row : ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(i==j || i+j==n+1){printf("*");}
+            else{printf(" ");}
+        }
+        printf("\n");
+    }
+    return 0;
+}
